b77a8f7b37e576879069716ce4bee027b1a00b22
Tony Hirst <tony.hirst@gmail.com>
2019-03-11 17:51:58 +0000

    Create jupyter_styling.sh

---
+++ jupyter_custom_files/jupyter_styling.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# EVERYTHING BELOW HERE SHOULD JUST WORK....
+
+
+#------- PROFILE: tm112MT -------
+#------- Jupyter does not have a notion of profiles -------
+
+###
+
+JUPYTERCONFIGDIR=$(jupyter --config-dir)
+
+
+echo "Ensuring default jupyter directories available"
+#Ensure directories are available
+mkdir -p $JUPYTERCONFIGDIR
+
+
+echo "Ensuring required jupyter sub-directories available"
+mkdir -p $JUPYTERCONFIGDIR/custom
+
+#Styling and branding extensions
+echo "Ensuring jupyter customisation files available"
+cp $THISDIR/custom/* $JUPYTERCONFIGDIR/custom/
