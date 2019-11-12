fcb82190db7c7c03af86639b21f4c4787114e54f
Tony Hirst <tony.hirst@gmail.com>
2019-11-12 18:22:35 +0000

    Improve reporting diagnostics during build; add nbextensions

--- jupyter_custom_files/jupyter_styling.sh
+++ jupyter_custom_files/jupyter_styling.sh
@@ -16,10 +16,11 @@ JUPYTERCONFIGDIR=$(jupyter --config-dir)
 echo "Ensuring default jupyter directories available"
 #Ensure directories are available
 mkdir -p $JUPYTERCONFIGDIR
-
+echo "Created dir: $JUPYTERCONFIGDIR"
 
 echo "Ensuring required jupyter sub-directories available"
 mkdir -p $JUPYTERCONFIGDIR/custom
+echo "Created dir: $JUPYTERCONFIGDIR/custom"
 
 #Styling and branding extensions
 echo "Ensuring jupyter customisation files available"
